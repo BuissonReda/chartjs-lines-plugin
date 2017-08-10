85cd97649ddc99b36df547838ed09c39d8114c26
Réda Buisson <buisson_reda@yahoo.fr>
2017-08-10 11:18:51 +0100

    Allow x value for horizontal lines and y value for vertical lines

--- dist/chartjs-lines-plugin.js
+++ dist/chartjs-lines-plugin.js
@@ -1,91 +1,94 @@
-Chart.pluginService.register({
-    afterDraw: function(chartInstance) {
-        var yValue;
-        var yScale = chartInstance.scales["y-axis-0"];
-        var canvas = chartInstance.chart;
-        var ctx = canvas.ctx;
-        var index;
-        var line;
-        var style;
-
-        if (chartInstance.options.horizontalLine) {
-            for (index = 0; index < chartInstance.options.horizontalLine.length; index++) {
-                line = chartInstance.options.horizontalLine[index];
-
-                if (!line.style) {
-                    style = "rgba(169,169,169, .6)";
-                } else {
-                    style = line.style;
-                }
-
-                if (line.y) {
-                    yValue = yScale.getPixelForValue(line.y);
-                } else {
-                    yValue = 0;
-                }
-
-                ctx.lineWidth = 3;
-
-                if (yValue) {
-                    ctx.beginPath();
-                    ctx.moveTo(60, yValue);
-                    ctx.lineTo(canvas.width, yValue);
-                    ctx.strokeStyle = style;
-                    ctx.stroke();
-                }
-
-                if (line.text) {
-                    ctx.fillStyle = style;
-                    ctx.fillText(line.text, 0, yValue + ctx.lineWidth);
-                }
-            }
-            return;
-        }
-    }
-});
+function getPixelForXValue (x, xScale) {
+  return x * (xScale.width / xScale.ticks.length) + xScale.left
+}
 
 Chart.pluginService.register({
-    afterDraw: function(chartInstance) {
-        var xValue;
-        var xScale = chartInstance.scales["x-axis-0"];
-        var canvas = chartInstance.chart;
-        var ctx = canvas.ctx;
-        var index;
-        var line;
-        var style;
+  afterDraw: function (chartInstance) {
+    var xValue
+    var yValue
+    var xScale = chartInstance.scales['x-axis-0']
+    var yScale = chartInstance.scales['y-axis-0']
+    var canvas = chartInstance.chart
+    var ctx = canvas.ctx
+    var index
+    var line
+    var style
 
-        if (chartInstance.options.verticalLine) {
-            for (index = 0; index < chartInstance.options.verticalLine.length; index++) {
-                line = chartInstance.options.verticalLine[index];
+    if (chartInstance.options.horizontalLine) {
+      for (index = 0; index < chartInstance.options.horizontalLine.length; index++) {
+        line = chartInstance.options.horizontalLine[index]
 
-                if (!line.style) {
-                    style = "rgba(169,169,169, .6)";
-                } else {
-                    style = line.style;
-                }
+        if (line != null) {
+          if (!line.style) {
+            style = 'rgba(169,169,169, .6)'
+          } else {
+            style = line.style
+          }
+          xValue = line.x != null ? getPixelForXValue(line.x, xScale) : getPixelForXValue(0, xScale)
+          yValue = line.y != null ? yScale.getPixelForValue(line.y) : yScale.getPixelForValue(0)
 
-                if (line.x) {
-                    xValue = xScale.getPixelForValue(line.x);
-                } else {
-                    xValue = 0;
-                }
+          ctx.lineWidth = 3
 
-                ctx.lineWidth = 3;
+          if (xValue != null && yValue != null) {
+            ctx.beginPath()
+            ctx.moveTo(xValue, yValue)
+            ctx.lineTo(canvas.width - 15, yValue) // Another offset to fit the line with the canvas
+            ctx.strokeStyle = style
+            ctx.stroke()
+          }
 
-                if (xValue) {
-                    ctx.beginPath();
-                    ctx.moveTo(xValue,39);
-                    ctx.lineTo(xValue, canvas.height-45);
-                    ctx.strokeStyle = style;
-                    ctx.stroke();
-                }
-
-                if (line.text) {
-                    ctx.fillStyle = style;
-                    ctx.fillText(line.text, xValue + ctx.lineWidth, 0);
-                }
-            }
-            return;
+          if (line.text) {
+            ctx.fillStyle = style
+            ctx.fillText(line.text, 0, yValue + ctx.lineWidth)
+          }
         }
+      }
     }
-});
+  }
+})
+
+Chart.pluginService.register({
+  afterDraw: function (chartInstance) {
+    var xValue
+    var yValue
+    var xScale = chartInstance.scales['x-axis-0']
+    var yScale = chartInstance.scales['y-axis-0']
+    var canvas = chartInstance.chart
+    var ctx = canvas.ctx
+    var index
+    var line
+    var style
+
+    if (chartInstance.options.verticalLine) {
+      for (index = 0; index < chartInstance.options.verticalLine.length; index++) {
+        line = chartInstance.options.verticalLine[index]
+
+        if (line != null) {
+          if (!line.style) {
+            style = 'rgba(169,169,169, .6)'
+          } else {
+            style = line.style
+          }
+
+          xValue = line.x != null ? getPixelForXValue(line.x, xScale) : getPixelForXValue(0, xScale)
+          yValue = line.y != null ? yScale.getPixelForValue(line.y) : yScale.getPixelForValue(yScale.end)
+
+          ctx.lineWidth = 3
+
+          if (xValue != null && yValue != null) {
+            ctx.beginPath()
+            ctx.moveTo(xValue, yValue)
+            ctx.lineTo(xValue, canvas.height - 37)
+            ctx.strokeStyle = style
+            ctx.stroke()
+          }
+
+          if (line.text) {
+            ctx.fillStyle = style
+            ctx.fillText(line.text, xValue + ctx.lineWidth, 0)
+          }
+        }
+      }
+    }
+  }
+})
